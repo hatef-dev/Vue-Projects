1a7dc075a8aa627116b0282f0e095dd5ff07ab54
hatef-dev <hatef.dev21@gmail.com>
2024-07-05 14:21:24 +0330

    StoredResource added and LearningResources Added

---
+++ 01/src/components/learning-resource/StoredResources.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in resources"
+      :key="res.id"
+      :description="res.description"
+      :link="res.link"
+      :title="res.title"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from "./LearningResource.vue";
+export default {
+  components: {
+    LearningResource,
+  },
+  props: {
+    resources: {
+        type: Array,
+        required:true
+    }
+  }
+};
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
